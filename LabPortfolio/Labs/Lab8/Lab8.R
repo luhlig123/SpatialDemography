7c733227a9c0fbb560661adbcfc8af9faa62ae5f
luhlig123 <leo.r.uhlig@gmail.com>
2026-05-20 13:54:06 -0700

    walker chapter done

--- LabPortfolio/Labs/Lab8/Lab8.R
+++ LabPortfolio/Labs/Lab8/Lab8.R
@@ -10,7 +10,13 @@
 
 library(tidyverse)
 library(tidycensus)
+library(sf)
+library(patchwork)
+library(units)
+library(corrr)
+library(car)
 
+#LAUDET AND IMAI CHAPTER 4
 #country gdp data
 co <- read.csv("countries.csv")
 
@@ -69,3 +75,149 @@ abline(light_gdp_fit)
 cor(co$gdp, co$prior_gdp)^2
 cor(co$log_gdp, co$log_prior_gdp)^2
 cor(co$gdp_change, co$light_change)^2
+
+#---------------------------------------
+#WALKER CHAPTER 8.2
+
+#dallas fortworth
+dfw_counties <- c("Collin County", "Dallas", "Denton",
+                  "Ellis", "Hunt", "Kaufman", "Rockwall",
+                  "Johnson", "Parker", "Tarrant", "Wise")
+
+variables_to_get <- c(
+  median_value = "B25077_001",
+  median_rooms = "B25018_001",
+  median_income = "DP03_0062",
+  total_population = "B01003_001",
+  median_age = "B01002_001",
+  pct_college = "DP02_0068P",
+  pct_foreign_born = "DP02_0094P",
+  pct_white = "DP05_0077P",
+  median_year_built = "B25037_001",
+  percent_ooh = "DP04_0046P"
+)
+
+dfw_data <- get_acs(
+  geography = "tract",
+  variables = variables_to_get,
+  state = "TX",
+  county = dfw_counties,
+  geometry = TRUE,
+  output = 'wide',
+  year = 2020
+) %>%
+  select(-NAME) %>%
+  st_transform(32138) #NAD83/TEXAS NORTH CENTRAL
+
+#Median Home Value Map
+mhv_map <- ggplot(dfw_data, aes(fill = median_valueE)) +
+  geom_sf(color = NA) +
+  scale_fill_viridis_c(labels = scales::label_dollar()) +
+  theme_void() +
+  labs(fill = "Median home value")
+
+#Median Home Value histogram
+mhv_histogram <- ggplot(dfw_data, aes(x = median_valueE)) +
+  geom_histogram(alpha = 0.5, fill = "navy", color = "navy", bins = 100) +
+  theme_minimal() +
+  scale_x_continuous(labels = scales::label_number(accuracy = 0.1)) +
+  labs(x = "Median home value")
+
+mhv_map + mhv_histogram
+
+#log maps
+mhv_map_log <- ggplot(dfw_data, aes(fill = log(median_valueE))) +
+  geom_sf(color = NA) +
+  scale_fill_viridis_c(labels = scales::label_dollar()) +
+  theme_void() +
+  labs(fill = "Median home value\nvalue (log)")
+
+mhv_histogram_log <- ggplot(dfw_data, aes(x = log(median_valueE))) +
+  geom_histogram(alpha = 0.5, fill = "navy", color = "navy", bins = 100) +
+  theme_minimal() +
+  scale_x_continuous(labels = scales::label_number(accuracy = 0.1)) +
+  labs(x = "Median home value (Log)")
+
+
+mhv_map_log + mhv_histogram_log
+
+
+#Feature engineering
+dfw_data_for_model <- dfw_data %>%
+  mutate(pop_density = as.numeric(set_units(total_populationE / st_area(.),"1/km2")),
+         median_structure_age = 2018 - median_year_builtE) %>%
+  select(!ends_with("M")) %>%
+  rename_with(.fn = ~str_remove(.x, "E$")) %>%
+  na.omit()
+
+
+#model
+formula <- paste0("log(median_value) ~ median_rooms + median_income + ",
+                  "pct_college + pct_foreign_born + pct_white + ",
+                  "median_age + median_structure_age + ",
+                  "percent_ooh + pop_density + total_population")
+
+model1 <- lm(formula = formula, data = dfw_data_for_model)
+
+summary(model1)
+
+#correlation matrix
+dfw_estimates <- dfw_data_for_model %>%
+  select(-GEOID, -median_value, -median_year_built) %>%
+  st_drop_geometry()
+
+correlations <- correlate(dfw_estimates, method = "pearson")
+network_plot(correlations)
+
+vif(model1)
+
+#Model2
+formula2 <- paste0("log(median_value) ~ median_rooms + pct_college + ",
+                   "pct_foreign_born + pct_white + median_age + ",
+                   "median_structure_age + percent_ooh + pop_density + ",
+                   "total_population")
+model2 <- lm(formula = formula2, data = dfw_data_for_model)
+summary(model2)
+
+vif(model2)
+
+
+#PCA
+pca <- prcomp(
+  formula = ~.,
+  data = dfw_estimates,
+  scale. = TRUE,
+  center = TRUE
+)
+summary(pca)
+
+
+pca_tibble <- pca$rotation %>%
+  as_tibble(rownames = "predictor")
+
+pca_tibble %>%
+  select(predictor:PC5) %>%
+  pivot_longer(PC1:PC5, names_to = "component", values_to = "value") %>%
+  ggplot(aes(x = value, y = predictor)) +
+  geom_col(fill = "darkgreen", color = "darkgreen", alpha = 0.5) +
+  facet_wrap(~component, nrow = 1) +
+  labs(y = NULL, x = "Value") +
+  theme_minimal()
+
+components <- predict(pca, dfw_estimates)
+
+dfw_pca <- dfw_data_for_model %>%
+  select(GEOID, median_value) %>%
+  cbind(components)
+
+ggplot(dfw_pca, aes(fill = PC1)) +
+  geom_sf(color = NA) +
+  theme_void() +
+  scale_fill_viridis_c()
+
+pca_formula <- paste0("log(median_value) ~ ",
+                      paste0('PC', 1:6, collapse = " + "))
+
+pca_model <- lm(formula = pca_formula, data = dfw_pca)
+
+summary(pca_model)
